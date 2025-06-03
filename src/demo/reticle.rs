26bd5c05a2d38d12a1ee252850c6c4503be3a921
Sam Baskin <sambskn@gmail.com>
2025-06-02 21:48:49 -0600

    basic reticle movement

--- src/demo/reticle.rs
+++ src/demo/reticle.rs
@@ -3,7 +3,8 @@ use bevy::{
     prelude::*,
 };
 
-use crate::asset_tracking::LoadResource;
+use super::shooting::ShootingController;
+use crate::{AppSystems, PausableSystems, asset_tracking::LoadResource};
 
 pub(super) fn plugin(app: &mut App) {
     app.register_type::<Reticle>();
@@ -12,16 +13,41 @@ pub(super) fn plugin(app: &mut App) {
     app.load_resource::<ReticleAssets>();
 
     // TODO: add systems for:
-    // - updating reticle postition based on ShootingController target
     // - update reticle visual over time
+    app.add_systems(
+        Update,
+        (update_reticle_visual_wave_val_timer, update_reticle_target)
+            .chain()
+            .run_if(resource_exists::<ReticleAssets>)
+            .in_set(AppSystems::TickTimers)
+            .in_set(PausableSystems),
+    );
+
+    app.add_systems(
+        Update,
+        update_reticle_position
+            .run_if(resource_exists::<ReticleAssets>)
+            .in_set(AppSystems::Update)
+            .in_set(PausableSystems),
+    );
 }
 
-// TODO: Add spawner for Reticle to call in level init
+pub fn reticle(reticle_assets: &ReticleAssets) -> impl Bundle {
+    (
+        Sprite {
+            image: reticle_assets.texture.clone(),
+            ..default()
+        },
+        Transform::default().with_scale(Vec3::splat(2.0)),
+        Reticle::default(),
+    )
+}
 
 #[derive(Component, Debug, Clone, Copy, PartialEq, Reflect)]
 #[reflect(Component)]
 struct Reticle {
-    pub speed: f32,
+    pub move_speed: f32,
+    pub visual_speed: f32,
     pub visual_wave_val: f32,
     pub target: Option<Vec2>,
 }
@@ -29,7 +55,8 @@ struct Reticle {
 impl Default for Reticle {
     fn default() -> Self {
         Self {
-            speed: 50.0,
+            move_speed: 450.0,
+            visual_speed: 5.0,
             visual_wave_val: 0.5,
             target: None,
         }
@@ -54,6 +81,41 @@ impl FromWorld for ReticleAssets {
                     settings.sampler = ImageSampler::nearest();
                 },
             ),
+        }
+    }
+}
+
+fn update_reticle_visual_wave_val_timer(mut reticle_query: Query<&mut Reticle>, time: Res<Time>) {
+    for mut reticle in reticle_query.iter_mut() {
+        reticle.visual_wave_val += time.delta_secs() * reticle.visual_speed;
+        while reticle.visual_wave_val > 1.0 {
+            reticle.visual_wave_val -= 1.0;
+        }
+    }
+}
+
+fn update_reticle_target(
+    mut reticle_query: Query<&mut Reticle>,
+    shooting_controller_query: Query<(&Transform, &ShootingController)>,
+) {
+    for mut reticle in reticle_query.iter_mut() {
+        if let Some((transform, shooting_controller)) = shooting_controller_query.iter().next() {
+            reticle.target =
+                Some(transform.translation.truncate() + shooting_controller.target_offset);
+        }
+    }
+}
+
+fn update_reticle_position(mut reticle_query: Query<(&mut Transform, &Reticle)>, time: Res<Time>) {
+    for (mut transform, reticle) in reticle_query.iter_mut() {
+        if let Some(target) = reticle.target {
+            let direction = target - transform.translation.truncate();
+            let distance = direction.length();
+            if distance > 0.0 {
+                let step = time.delta_secs() * reticle.move_speed;
+                let step = step.min(distance);
+                transform.translation += (direction.normalize() * step).extend(1.0);
+            }
         }
     }
 }
